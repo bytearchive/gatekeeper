8a5f06c33fab4d2345efb8449c94188c1a32983f
Liyan Chang <liyanchang@mit.edu>
2013-03-27 00:44:21 -0700

    using a file change based config file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import yaml
+import os
 
 from flask import Flask, render_template, Response, request
 
@@ -10,20 +11,41 @@ logging.basicConfig(filename='access.log', filemode='w', level=logging.INFO)
 class Config(dict):
     def __init__(self, config_path='config.yaml'):
         self.config_path = config_path
+        self.file_hash = os.path.getmtime(config_path)
         super(Config, self).__init__(yaml.load(open(config_path)))
         logging.info("CONFIG: " + str(self))
 
-    def __getitem__(self, key):
+    def _file_changed(self):
+        new_hash = os.path.getmtime(self.config_path)
+        change = self.file_hash != new_hash
+        self.file_hash = new_hash
+        return change
+
+    def _reload(self):
+        if not self._file_changed():
+            return
+
         # reload variables everytime so you don't have to restart server
-        # goal is also not to crash on bad config so you don't get locked out
+        # goal is also not to crash on bad config
         try:
             new_config = yaml.load(open(self.config_path))
             super(Config, self).__init__(new_config)
             logging.debug("CONFIG: " + str(self))
         except yaml.parser.ParserError:
             logging.error('Configuration File Parse Error. Still using previous file')
+
+    def __repr__(self):
+        self._reload()
+        return dict.__repr__(self)
+
+    def __getitem__(self, key):
+        self._reload()
         # Capitalize since Yaml looks better with caps and python better in lower case.
         return super(Config, self).__getitem__(key.capitalize())
+
+    def __setitem__(self, key, val):
+        raise NotImplementedError
+
 
 config = Config()
 door_timeout = 0
