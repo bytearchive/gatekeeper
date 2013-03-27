fd1c6e1757df1e07ccdf9a5daa2e690cabbcb392
Liyan Chang <liyanchang@mit.edu>
2013-03-27 01:32:51 -0700

    heroku procfile and port config

--- app.py
+++ app.py
@@ -82,4 +82,5 @@ def get_number():
     return Response(response, mimetype='text/xml')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
